8bddf5013df570434202e5b056d3e5f37dd0fff5
Gaurav Dalvi <gdalvi20dec@gmail.com>
2023-07-18 17:41:06 +0530

    update contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,9 +16,49 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_id',
+        'template_id',
+        {
+          from_name: form.name,
+          to_name: 'Jack',
+          from_email: form.email,
+          to_email: 'email',
+          message: form.message,
+        },
+        'public_key'
+      )
+      .then(() => {
+        setLoading(false);
+        alert('Thank you. I will get back to you as soon as possible.');
+
+        setForm(
+          {
+            name: '',
+            email: '',
+            message: '',
+          },
+          (error) => {
+            setLoading(false);
+
+            console.log(error);
+
+            alert('Something went wrong.');
+          }
+        );
+      });
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
